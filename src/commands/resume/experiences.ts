bb9ccd578a919f124d8baea8599ba9d740c81a41
rferre <robin.ferre@gmail.com>
2026-04-01 00:32:30 +0200

    feat(assets,experiences): update resume

--- src/commands/resume/experiences.ts
+++ src/commands/resume/experiences.ts
@@ -1,90 +1,132 @@
 export const experiences = `
-<p>Freelance fullstack engineer</p>
+<p>Freelance Software Engineer</p>
+<p>FDJ UNITED</p>
+<p>Boulogne, France</p>
+<p>December 2024 - Present</p>
+<ul>
+  <li>Staffing / Project Team: 11 people</li>
+  <li>Context: FDJ UNITED is La Française des Jeux, a French lottery and sports betting company.</li>
+  <li>I work on the development of games and tickets for FDJ United's international lottery partners.</li>
+</ul>
+<p>Technical environment</p>
+<ul>
+  <li>NodeJS, TypeScript, Algorithms, Geometry, Statistics</li>
+</ul>
+<p>--------------------------</p>
+<p>--------------------------</p>
+<p>Freelance Fullstack Engineer</p>
+<p>Midgard</p>
+<p>Paris/Ajaccio, France</p>
+<p>March 2023 - August 2024</p>
+<ul>
+  <li>Staffing / Project Team: 4 people</li>
+  <li>Context: Midgard is a ~10-person GreenTech start-up offering innovative solutions to address natural hazards, save lives, and protect natural and urban spaces.</li>
+  <li>I worked on the fullstack development of a forest and urban fire detection application (real-time AI smoke detection, fire mapping, alert management, camera streaming, media management, user management — mainly firefighters). I also mentored junior profiles.</li>
+</ul>
+<p>Tasks</p>
+<ul>
+  <li>Microservices architecture</li>
+  <li>General design from scratch, project initialization, story development and optimizations</li>
+  <li>Project quality: integration / unit tests (50% coverage), 2-reviewer code reviews</li>
+  <li>Collaboration with business stakeholders: requirements gathering, story writing, documentation</li>
+  <li>Pre-sales and technical follow-up with clients</li>
+  <li>Project management: meeting facilitation, business workshops, planning and tracking</li>
+  <li>Bug fixes, optimizations, and feature development</li>
+  <li>Agile organization: 2-week sprints, Scrum methodology, ceremonies, JIRA</li>
+  <li>Technical documentation (API docs, release notes automation, etc.)</li>
+  <li>Functional / technical presentations during company reviews and client meetings</li>
+  <li>Proof of Concepts (POCs)</li>
+</ul>
+<p>Technical environment</p>
+<ul>
+  <li>NodeJS, NestJS, Angular, TypeScript, Mongoose, Jest, MongoDB, RabbitMQ, REST API, Websocket, Python, Docker, Kubernetes, AWS, Jira, Bitbucket, GitLab, Visual Studio Code, Linux, AI, TensorFlow, Image Processing</li>
+</ul>
+<p>--------------------------</p>
+<p>--------------------------</p>
+<p>Freelance Fullstack Engineer</p>
 <p>Lemonway</p>
 <p>Paris, France</p>
 <p>April 2022 - January 2023</p>
 <ul>
-  <li>Management / Project team: 7 people.</li>
-  <li>Context: Lemonway is a scale-up of about 150 people. As a fintech, it offers payment solutions dedicated to marketplaces (B2B, B2C and C2C), participative financing platforms as well as to corporates requiring payment and fundraising services on behalf of third parties in a secure and regulated framework (KYC). </li>
-  <li>I work on the fullstack development of the application used by analysts to process and verify the tens of millions of Lemonway users (customer/merchant identification, fight against financial crimes and money laundering).</li>
+  <li>Staffing / Project Team: 7 people</li>
+  <li>Context: Lemonway is a ~150-person Fintech scale-up offering payment solutions for marketplaces (B2B, B2C and C2C), crowdfunding platforms, and corporates requiring payment and fund collection services within a secure, regulated framework (KYC).</li>
+  <li>I worked on the fullstack development of the application used by analysts to process and verify tens of millions of Lemonway users (client/merchant identification, anti-financial crime and anti-money laundering).</li>
 </ul>
 <p>Tasks</p>
 <ul>
-  <li>Micro-services architecture</li>
-  <li>General design, story development and optimization</li>
-  <li>Project quality : integration tests / unit tests (90\% coverage), reviews 2 proofreaders, SonarQube, 0 warnings, 0 messages</li>
-  <li>Collaboration with the PO: collection of needs, writing stories, global documentation under Notion</li>
-  <li>Project management: facilitation of PPP meetings, business workshops, subdivision and project follow-up</li>
-  <li>Realization of corrections, optimizations, evolutions</li>
-  <li>Agile organization: 2-week sprint, Scrum method, ceremonies, JIRA</li>
-  <li>Writing of documentation (technical, api, release automation, etc.)</li>
-  <li>Functional / technical presentations during company reviews or to customers</li>
-  <li>Realization of POCs</li>
+  <li>Microservices architecture</li>
+  <li>General design, story development and optimizations</li>
+  <li>Project quality: integration / unit tests (90% coverage), 2-reviewer code reviews, SonarQube, 0 warnings, 0 messages</li>
+  <li>Collaboration with PO: requirements gathering, story writing, global documentation in Notion</li>
+  <li>Project management: PPP-format meeting facilitation, business workshops, planning and tracking</li>
+  <li>Bug fixes, optimizations, and feature development</li>
+  <li>Agile organization: 2-week sprints, Scrum methodology, ceremonies, JIRA</li>
+  <li>Technical documentation (API docs, release notes automation, etc.)</li>
+  <li>Functional / technical presentations during company reviews and client meetings</li>
+  <li>Proof of Concepts (POCs)</li>
 </ul>
 <p>Technical environment</p>
 <ul>
-  <li>NodeJS, NestJS, VueJS, Saas, Typescript, Mongoose, Jest, MongoDB, Mongo Atlas Search Index, RabbitMQ, Docker, SonarQube, AWS, Jira, GitLab, Notion, Visual Studio Code</li>
+  <li>NodeJS, NestJS, VueJS, Sass, TypeScript, Mongoose, Jest, MongoDB, Mongo Atlas Search Index, RabbitMQ, REST API, Websocket, Docker, SonarQube, AWS, Jira, GitLab, Notion, Visual Studio Code</li>
 </ul>
 <p>--------------------------</p>
 <p>--------------------------</p>
-<p>Full stack engineer, Data scientist</p>
+<p>Fullstack Engineer, Data Scientist</p>
 <p>Transway</p>
 <p>Nantes, France</p>
 <p>February 2020 - February 2022</p>
 <ul>
-  <li>Engineer on several projects for Transway, a 25-person start-up promoting ecomobility. The different projects aim to boost the use of public transport, cycling, carpooling and walking. I worked on the development of a multi-tenant PaaS for loyalty programmes, around a movement tracker, on the R\&D on MaaS (mobility as a service), on the coaching of junior profiles and then on the implementation of the management, processing and visualisation/reporting of data for all Transway's projects.</li>
+  <li>Engineer on multiple projects at Transway, a 25-person start-up promoting eco-mobility. Projects aimed at boosting the use of public transport, cycling, carpooling and walking. I worked on the development of a multi-tenant PaaS for loyalty programs around a movement tracker, R&D on MaaS (Mobility as a Service), mentoring of junior profiles, and the implementation of data management, processing and visualization/reporting for all Transway projects.</li>
 </ul>
 <p>Tasks</p>
 <ul>
-  <li>Design of the application (architecture document, workshop facilitation with the teams)</li>
-  <li>Front-end development (VueJS, Sass, typescript)</li>
-  <li>Back-end development (NodeJS, NestJS, typescript, pm2, postgresql)</li>
-  <li>TU/TI (cypress on the front, jest for the back, integration with deployment pipelines)</li>
-  <li>Application maintenance (fix, hotfix, alert management)</li>
-  <li>Conception et maintenance du CI/CD (pipelines, cloud)</li>
+  <li>Application design (architecture documents, workshop facilitation with teams)</li>
+  <li>Frontend development (VueJS, Sass, TypeScript)</li>
+  <li>Backend development (NodeJS, NestJS, TypeScript, pm2, PostgreSQL)</li>
+  <li>Unit/integration tests (Cypress on frontend, Jest for backend, integration into deployment pipelines)</li>
+  <li>Application maintenance (fixes, hotfixes, alert management)</li>
+  <li>CI/CD design and maintenance (pipelines, cloud)</li>
   <li>Server management</li>
-  <li>Documentation redaction (technical, api, release automation, etc)</li>
-  <li>Pre-sales, technical follow-up with customers</li>
-  <li>Functional / technical presentations at company reviews or to customers</li>
-  <li>Technical supervision of work of students, trainees and juniors</li>
-  <li>POCs</li>
-  <li>Setting up an ETL (apache nifi)</li>
-  <li>Import and processing via ETL in an S3 datalake (aws) of data from all projects (10 bdds)</li>
-  <li>Development of data models</li>
-  <li>Connecting a data-vizualisation tool to the datalake (Tableau)</li>
-  <li>Creation of real-time updated KPIs</li>
+  <li>Technical documentation (API docs, release notes automation, etc.)</li>
+  <li>Pre-sales and technical follow-up with clients</li>
+  <li>Functional / technical presentations during company reviews and client meetings</li>
+  <li>Technical mentoring of apprentices, interns, and juniors</li>
+  <li>Proof of Concepts (POCs)</li>
+  <li>ETL setup (Apache NiFi)</li>
+  <li>Import and processing via ETL into an AWS S3 data lake for all projects (10 databases)</li>
+  <li>Data model design</li>
+  <li>Connection of a data visualization tool to the data lake (Tableau)</li>
+  <li>Creation of real-time KPIs</li>
 </ul>
 <p>Technical environment</p>
 <ul>
-<li>NodeJS, Typescript, NestJS, VueJS, Saas, Pm2, Postgresql, Typeorm, Mongodb, Mongoose, MySQL, Cypress, Jest, Sentry, Docker, Bitbucket-pipeline, Gitlab CI, Linux, OVH, Clevercloud</li>
-<li>Apache Nifi, Amazon S3, Amazon Athena, Tableau</li>
+  <li>NodeJS, TypeScript, NestJS, VueJS, Sass, Pm2, PostgreSQL, TypeORM, MongoDB, Mongoose, MySQL, Cypress, Jest, Sentry, Docker, Bitbucket-pipeline, GitLab CI, Linux, OVH, Clevercloud, Apache NiFi, Amazon S3, Amazon Athena, Tableau</li>
 </ul>
 <p>--------------------------</p>
 <p>--------------------------</p>
-<p>Software engineer</p>
+<p>Software Engineer</p>
 <p>Airbus Defense and Space</p>
 <p>Elancourt, France</p>
 <p>October 2018 - February 2020</p>
 <ul>
-  <li>
-  Developer and integrator on the back-end of the project Massive Intelligence. International project with 10 other teams in agile mode. This project aims to bring together all intelligence services on a single platform (Open Source, Satellite Imaging, Intelligence...). Data is ingested, processed and visualised via micro-services. The product will be deployed in the cloud or on site depending on the client's needs.
-  </li>
+  <li>Backend developer and integrator on the Massive Intelligence project. International project with 10 other teams in Agile mode. The project aims to consolidate all intelligence services on a single platform (Open Source, Satellite Imagery, Intelligence...). Data is ingested, processed and visualized via microservices. The product is deployed in the cloud or on-premise depending on client needs.</li>
 </ul>
 <p>Tasks</p>
 <ul>
-  <li>Job management, Management of processing chains, User management, SSO, REST API services to interface COTS service with UI, Creation of reports YGWYS...</li>
-  <li>Documentation writing: design documents, APIs, requirements, releases notes, licenses, architectural documents</li>
-  <li>Integration of services developed on a development platform, use of Ansible and Terraform scripting to automate deployment, Continuous Delivery with Jenkins based on tests on a platform that is recreated every day</li>
-  <li>Maintenance of the services developed, updating with bug fixing according to the evolution of the need</li>
-  <li>Intégration d’outils propriétaires à la plateforme : Nifi, Dataiku, MongoDB, Kafka, ELK, Minio S3...</li>
-  <li>Writing TU/TI: Postman test scripts, called during continuous delivery</li>
-  <li>Accompaniment of other teams on Docker, help on some Ansible scripts, on Java</li>
+  <li>Design and development of backend services</li>
+  <li>Task management, processing chain management, user management, SSO, REST API services to interface proprietary services with the UI, report generation via an online editor</li>
+  <li>Documentation writing: design documents, API, requirements, release notes, licenses, architectural documents</li>
+  <li>Integration of developed and dockerized services on a development platform, Ansible and Terraform scripts to automate deployment, continuous delivery with Jenkins based on tests on a platform rebuilt daily</li>
+  <li>Maintenance of developed services, updates and bug fixes in line with evolving requirements</li>
+  <li>Integration of proprietary tools on the platform: NiFi, Dataiku, MongoDB, Kafka, ELK, Minio S3...</li>
+  <li>Unit/integration test writing: delivery test scripts called during continuous delivery</li>
+  <li>Support for other teams on Docker, Ansible scripts, and Java</li>
   <li>POC around a Cassandra cluster (Datastax suite, Cassandra, Spark)</li>
-  <li>Production of videos, slides around the platform, real-time presentations of the platform to sales, marketing and product owner</li>
+  <li>Videos and slides about the platform, live platform demos to sales, marketing and POs</li>
 </ul>
 <p>Technical environment</p>
 <ul>
-<li>Java, Springboot, micro-service, docker, Nifi, Dataiku, Mongodb, ELK, Minio S3, Postman TU</li>
+  <li>Java, Spring Boot, Microservices, Docker, NiFi, Dataiku, MongoDB, ELK, Minio S3, Postman, Unit Tests</li>
 </ul>
 <p>--------------------------</p>
 <p>--------------------------</p>
